aa8424ebcc1c49b659045c25742bd25be1a607df
Diome Abdoulaye <lextimide@gmail.com>
2025-10-27 23:09:46 +0000

    Move admin seeder to DatabaseSeeder for proper execution order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminSeeder::class,  // Admin en premier
             ClientSeeder::class,
-            AdminSeeder::class,
             CompteSeeder::class,
             TransactionSeeder::class,
         ]);
